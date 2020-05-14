0f6093cbcae9d91aaf806cf86d43a278295022a5
PNfeather <921192114@qq.com>
2020-05-14 16:25:03 +0800

    指令快速提交代码

--- routes/dbhandler.js
+++ routes/dbhandler.js
@@ -1,7 +1,7 @@
 var mongo=require("mongodb");
 var MongoClient = mongo.MongoClient;
 var assert = require('assert');
-var host="172.16.16.4";
+var host="182.61.147.202";
 var port="9080";
 var Urls = 'mongodb://' + host + ':' + port + '/PNfeather';
 // PNfeather  ===> 手动创建一个
